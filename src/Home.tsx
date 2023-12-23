cfbbe388e33b3bd415490f5287f12ee21eef2333
wodeni <wn2155@columbia.edu>
2023-12-23 00:06:37 -0500

    feat: minimal search engine

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,13 +1,25 @@
+import React from "react";
 import Gallery from "./Gallery";
 import Header from "./Header";
 import Search from "./Search";
-import { diagrams } from "./main";
+import { allDiagrams, miniSearch } from "./main";
 
 function Home() {
+  const [diagrams, setDiagrams] = React.useState(allDiagrams);
   return (
     <div className="container mx-auto flex flex-col justify-center items-center m-8">
       <Header />
-      <Search />
+      <Search
+        index={miniSearch}
+        onFound={(res) => {
+          const indices = res.map(({ id }) => id);
+          // if no results, default to all
+          if (res.length === 0) setDiagrams(allDiagrams);
+          else {
+            setDiagrams(allDiagrams.filter(({ id }) => indices.includes(id)));
+          }
+        }}
+      />
       <Gallery diagrams={diagrams} />
     </div>
   );
